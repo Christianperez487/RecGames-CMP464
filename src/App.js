e405b718a50fba99f38a3414a32d2102826aeb05
Christianperez487 <99904963+Christianperez487@users.noreply.github.com>
2023-05-20 22:41:01 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import logo from './placeholder.png';
 import './App.css';
+import Button from './components/Button';
+import Start from './components/Start';
+
 
 function App() {
+
+  const [buttonClicked, setButtonClicked] = useState(null)
+
+  const notifyStatus = (text) => {
+    console.log('Button was clicked')
+    console.log('Which one: ', text)
+
+    setButtonClicked(text)
+  }
+
+  const renderPage = () => {
+    console.log('Loading')
+    if(buttonClicked === 'Start') {
+      return <Start />
+    }
+else {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App">
+    <h1>RecGames</h1>
+    <img src= {logo} className="App-logo" alt="logo" />
+    <p>Your own personalized list so you won't waste time conversing.</p>
+    <Button text={'Start'} onButtonClick={notifyStatus} />
     </div>
-  );
+  )
+    }
+  }
+  return <div className='App'>{renderPage()}</div>
 }
 
 export default App;
