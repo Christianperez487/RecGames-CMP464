357cc93643012b6b148e44e73454626e3b66f5da
Christianperez487 <99904963+Christianperez487@users.noreply.github.com>
2023-05-21 21:03:13 -0400

    Updated Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,49 @@
 import { useState } from "react";
+import ReactDOM  from "react";
 
 
 const Form = (props) => {
-    const [gName, setGname] = useState("")
-    const [Genre, setGenre] = useState("")
+    const [game, setGame] = useState({});
 
+const handleChange = (event) => {
+    const game = event.target.game;
+    const gen = event.target.gen;
+
+    setGame(values => ({...values, [game]: gen}))
+ }
+  
+ const submission = (event) => {
+    event.preventDefault();
+    console.log(game);
+ }
+ return (
+    <form onSubmit={submission}>
+        <label>Name of Game:
+            <input
+              type="text"
+              name="Gname"
+              value={game.Gname || ""}
+              onChange={handleChange}
+            />
+
+        </label>
+        <label>Genre of Game:
+            <input
+            type="text"
+            name="Genre"
+            value={game.Genre || ""}
+            onChange={handleChange}
+             />
+
+        </label>
+        <input type="submit" />
+
+    </form>
+ )
 
 }
+
+const root = ReactDOM.createRoot(document.getElementsByName('root'));
+root.render(<Form />);
 
 export default Form
