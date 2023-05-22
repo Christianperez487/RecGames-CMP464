356085ed2c8301e0083a16c63811f95406da7e1d
Christianperez487 <99904963+Christianperez487@users.noreply.github.com>
2023-05-21 21:04:20 -0400

    Updated Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,19 +1,44 @@
 import React from 'react'
 
 
-const Menu = () => {
+const Header = () => {
+    
     return (
-        <thead>
+        <table>
             <tr>
-                <th>Game Name</th>
-
+                <th width={'30%'} style={{fontWeight:500,marginLeft:'90px'}}>Game</th>
                 <th>Genre</th>
-
-                <th>Description</th>
-                
+                <th>Delete</th>    
             </tr>
-        </thead>
+        </table>
     )
 }
 
-export default Menu
+const Body = (props) => {
+    const rows = props.linkData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.gName}</td>
+                <td>
+                    <a href={row.Genre}>{row.Genre}</a>
+                </td>
+                <td>
+                    <button onClick={() => props.removeGame(index)}>Delete</button>
+                </td>
+
+            </tr>
+
+        )
+    })
+    return <tbody>{rows}</tbody>
+}
+
+const menu = (props) => {
+    return <table>
+        <Header />
+        <Body linkData={props.linkData} removeGame={props.removeGame}/>
+    </table>
+}
+
+
+export default menu
