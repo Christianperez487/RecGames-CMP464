4da9e241df64931edc7a721b07d53a9bd1b705f3
Christianperez487 <99904963+Christianperez487@users.noreply.github.com>
2023-05-23 04:55:27 -0400

    Updated index.js connecting to postgres

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
 const express = require("express")
 
+const bodyParser = require('body-parser')
+
 const PORT = process.env.PORT || 8001
 
 const app = express()
 
+const db = require('./queries')
+
+app.use(bodyParser.json())
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
 app.get('/', (req, res) => {
-    res.send("Hello Citizen, are you from Apple World?")
+    res.json("Hello Citizen, are you from Apple World?")
 });
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Greetings from Apple World" })
-});
+
+
+app.get('/gamer', db.getGamer)
+app.get('/ugamer/:id', db.getGamerById)
+app.post('/gamer', db.createGamer)
+app.put('/gamer/:id', db.updateGamer)
+app.delete('/gamer/:id', db.deleteGamer)
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`)
