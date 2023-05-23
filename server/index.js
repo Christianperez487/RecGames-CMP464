ada8aefc0a6c678be8a6645e253923335a790690
Christianperez487 <99904963+Christianperez487@users.noreply.github.com>
2023-05-23 05:27:34 -0400

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express")
 
-const bodyParser = require('body-parser')
-
 const PORT = process.env.PORT || 8001
 
 const app = express()
 
+const path = require('path')
+
 const db = require('./queries')
+
+const bodyParser = require('body-parser')
+
 
 app.use(bodyParser.json())
 
@@ -16,14 +19,19 @@ app.use(
   })
 )
 
+
 app.get('/', (req, res) => {
     res.json("Hello Citizen, are you from Apple World?")
+});
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Greetings from Apple World" })
 });
 
 
 
 app.get('/gamer', db.getGamer)
-app.get('/ugamer/:id', db.getGamerById)
+app.get('/gamer/:id', db.getGamerById)
 app.post('/gamer', db.createGamer)
 app.put('/gamer/:id', db.updateGamer)
 app.delete('/gamer/:id', db.deleteGamer)
